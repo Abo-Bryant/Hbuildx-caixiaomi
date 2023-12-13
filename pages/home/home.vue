e88c76aabfda58818b9154536f0a25a8807f4d8e
lb <484532503@qq.com>
2023-12-13 15:26:52 +0800

    历史赊欠录入页面布局

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,92 +1,34 @@
 <template>
   <view class="page">
-   home123
-   <view class="big">
-     <view class="a"></view>
-     <view class="b"></view>
-   </view>
+    
    <navigator :url="'/subpkg-order/order-provisional/order-provisional'" class="billing">
      开单
   </navigator>
-   <!-- <uni-icons custom-prefix="iconfont" type="icon-kaidan" size="60"></uni-icons> -->
   </view>
 </template>
 
 <script>
-import {getKindListRequest,getOrderDetailRequest} from '../../api/api.js'
-import {timestampToTime}  from '../../utils/index.js'
-  const dayjs = require("dayjs");
   export default {
     data() {
       return {
-      orderDetail:{}
       };
     },
     onLoad() {
-    this.getOrderDetail()
     },
     computed:{
    
     },
     methods: {
-    async getOrderDetail(){
-      this.orderDetail = await getOrderDetailRequest(75)
-      // console.log('this.orderDetail',this.orderDetail.createdAt)
-        // console.log(this.orderDetail.createdAt + "的时间戳为：" + timestamp);
-        console.log( '时间',timestampToTime(this.orderDetail.createdAt));
-        console.log( '时间',dayjs().format('YYYY-MM-DD HH:mm:ss'));
-     // const arr = [1,2,4,5,5,6,5]
-  //     let newarr = arr.filter(item=>item!==Math.max(...arr))
-  //     console.log(arr,Math.max(...newarr))
-  //     let c =  arr.map((item,index)=>{
-  //       if(item===Math.max(...newarr)){
-  //         return index
-  //       }
-  //     })
-  //      console.log(c)
-  // let vows = "aeiou"
-  // let str_test = "Hello World!"
-  
-
-  //      let array1  = vows.split('')
-  //      let array2 = str_test.split('')
-  //     // 找到相同的元素
-  //     let newarr = []
-  //     for(let i =0;i<array1.length;i++){
-  //       for(let j =0;j<array2.length;j++){
-  //         if(array1[i]===array2[j]){
-  //           newarr.push(array1[i])
-  //         }
-  //       }
-  //     }
-  //     console.log(newarr)
+ 
     }
     }
-  }
 </script>
 
 <style lang="scss">
-// @import "@/static/iconfont.css";
 .page{
-  .big{
-    width: 200px;
-    height: 200px;
-    background-color: red;
-   
-    // display: flex;
-    .a{
-      width: 80px;
-      height: 80px;
-      background-color: green;
-       margin: 0 auto;
-  }
-  .b{
-    margin: 0 auto;
-      width: 50px;
-      height: 50px;
-      background-color: green;
-  }
-  }
+  width: 100%;
+  height: 600px;
+  background-color: #f4f5f7;
   .billing{
     position: fixed;
     bottom: 40px;
--- subpkg-buyer/buyer-historyOwe/buyer-historyOwe.vue
+++ subpkg-buyer/buyer-historyOwe/buyer-historyOwe.vue
@@ -1,19 +1,201 @@
 <template>
-  <view>
-    哈喽
+  <view class="page">
+    <view class="buyer">
+      <view class="left">
+        客户
+      </view>
+      <view class="right">
+        <view class="name">
+          小李
+        </view>
+        <view class="">
+          下欠:<text style="color: #e88432;">1000元</text>
+        </view>
+        <view class="icon">
+          <uni-icons color="#9b9b9b" type="right" size="30"></uni-icons>
+        </view>
+      </view>
+    </view>
+    <view class="oweTime">
+      <view class="left">
+        赊欠时间
+      </view>
+      <view class="right">
+        <view class="time">
+          2023-12-01
+        </view>
+        <view class="icon">
+          <uni-icons color="#9b9b9b" type="right" size="30"></uni-icons>
+        </view>
+      </view>
+    </view>
+    <view class="oweMoney">
+     <view class="input">
+        <uni-easyinput v-model="value" placeholderStyle="color: #333" :inputBorder="false" :clearable="false" placeholder="赊欠金额"></uni-easyinput>
+     </view>
+     <view class="unit">
+       元
+     </view>
+    </view>
+    <view class="uploading">
+      <view class="one">
+        <view class="text">
+          上传赊欠记录
+        </view>
+        <view class="num">
+          0/9
+        </view>
+      </view>
+      <view class="two">
+        <view class="icon">
+          <uni-icons color="#d1d5d8" type="plusempty" size="50"></uni-icons>
+        </view>
+        <view class="text">
+          拍照上传
+        </view>
+      </view>
+    </view>
+    <view class="submit">
+      提交
+    </view>
   </view>
 </template>
 
 <script>
   export default {
+    options: {
+    			styleIsolation: 'shared', // 解除样式隔离
+    		},
     data() {
       return {
-        
+        value:''
       };
     }
   }
 </script>
 
 <style lang="scss">
-
+.page{
+  width: 100%;
+  height: 600px;
+  background-color: #f4f5f7;
+  .buyer{
+    width: 100%;
+    height: 60px;
+    background-color: #fff;
+    border-top: 2px solid #e4e4e4;
+    border-bottom: 1px solid #e4e4e4;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .left{
+      margin-left: 20px;
+    }
+    .right{
+      margin-right: 10px;
+      display: flex;
+      align-items: center;
+      .name{
+        margin-right: 5px;
+      }
+      .icon{
+        
+      }
+    }
+  }
+  .oweTime{
+    width: 100%;
+    height: 60px;
+    background-color: #fff;
+    // border-top: 2px solid #e4e4e4;
+    border-bottom: 1px solid #e4e4e4;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .left{
+      margin-left: 20px;
+    }
+    .right{
+      margin-right: 10px;
+      display: flex;
+      align-items: center;
+      .time{
+        // margin-right: 5px;
+      }
+      .icon{
+        
+      }
+    }
+  }
+  .oweMoney{
+    width: 100%;
+    height: 80px;
+    background-color: #fff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .input{
+      margin-left: 20px;
+      .uni-easyinput{
+        width: 175%;
+        .uni-easyinput__content{
+          background-color: #f4f5f7 !important;
+        }
+      }
+    }
+    .unit{
+      margin-right: 20px;
+    }
+  }
+  .uploading{
+    margin-top: 20px;
+    width: 100%;
+    // height: 150px;
+    background-color: #fff;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    .one{
+      // margin-top: 20px;
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 20px;
+      .text{
+        margin-left: 20px;
+      }
+      .num{
+        margin-right: 20px;
+      }
+    }
+    .two{
+      margin-left: 20px;
+      width: 100px;
+      height: 100px;
+      background-color: #f4f5f7;
+      display: flex;
+       flex-direction: column;
+       align-items: center;
+         justify-content: center;
+      .icon{
+        
+      }
+      .text{
+        color: #b8b9bd;
+      }
+    }
+  }
+  .submit{
+    position: fixed;
+    bottom: 0px;
+    left: 0px;
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    text-align: center;
+    background-color: #1d9d60;
+    color: #fff;
+    border-radius: 60px;
+    font-size: 24px;
+    font-weight: 700;
+  }
+}
 </style>
